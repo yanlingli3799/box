09b83ac742e26cccccee807e06c246aa4de3b0c1
liyanling <liyanling@didiglobal.com>
2019-06-16 21:09:31 +0800

    update demo

--- javas/algorithms/src/main/java/com/javas/algorithms/no4/yuesu/ProblemDemo2.java
+++ javas/algorithms/src/main/java/com/javas/algorithms/no4/yuesu/ProblemDemo2.java
@@ -1,11 +1,45 @@
 package com.javas.algorithms.no4.yuesu;
 
+import java.util.List;
+
 /**
- * 例题2：
- * 给定一个字典表 和一个起始单词 一个结果单词
- * 单词每次只能变换一个字母 变换后的单词必须是字典表内的单词 找出从起始单词变换到结果单词的最短路径 （Leecode)
- *
+ * 例题2：https://leetcode-cn.com/problems/word-ladder/
  */
 public class ProblemDemo2 {
+
+  public static void main(String[] args) {
+
+    System.out.println(dist1("abc","abx"));
+    System.out.println(dist1("abc","axc"));
+    System.out.println(dist1("abc","xbc"));
+    System.out.println(dist1("axc","xbc"));
+    System.out.println(dist1("xbc","xbc"));
+  }
+
+  public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+
+
+
+      return 0;
+
+  }
+
+  // 判断两个字符串是不是只相差一个字符
+  public static boolean dist1(String A,String B){
+    if(A.length()!=B.length()){
+      return false;
+    }
+    int times = 0;
+    for(int i=0;i<A.length();i++){
+      if(A.charAt(i) !=B.charAt(i)){
+        times++;
+        if(times>1){
+          return false;
+        }
+      }
+    }
+    return times == 1;
+  }
+
 
 }
